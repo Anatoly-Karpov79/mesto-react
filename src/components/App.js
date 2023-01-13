731ef6b5d499a041723793e872047cc8f181da47
Anatoly Karpov <karpik-ne@yandex.ru>
2023-01-13 08:07:22 +0700

    ПР11: лайк и дизлайк

--- src/components/App.js
+++ src/components/App.js
@@ -42,10 +42,18 @@ function App() {
     // Снова проверяем, есть ли уже лайк на этой карточке
     const isLiked = card.likes.some((i) => i._id === currentUser._id);
 
-    // Отправляем запрос в API и получаем обновлённые данные карточки
-    api.setLike(card._id, !isLiked).then((newCard) => {
-      setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
-    });
+    if (isLiked === false)
+      api.setLike(card._id, !isLiked).then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card._id ? newCard : c))
+        );
+      });
+    else
+      api.removeLike(card._id, !isLiked).then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card._id ? newCard : c))
+        );
+      });
   }
 
   function handleCardDelete(card) {
